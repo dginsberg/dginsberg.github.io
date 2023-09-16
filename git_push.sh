4a06170f1990656b435e4648b021609a63f8dac0
dginsberg <55890498+dginsberg@users.noreply.github.com>
2023-09-16 12:30:44 -0400

    updated git push command

--- git_push.sh
+++ git_push.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Print the current git state
+# Display the current Git status
 git status
 
-# Add all changes
-git add --all
+# Check if the branch is up to date with the remote
+if git pull --rebase --quiet; then
+  echo "Your branch is already up to date with the remote."
+else
+  # Add all changes
+  git add --all
 
-# Prompt for a commit message
-read -p "Enter a commit message: " commit_message
+  # Prompt for a commit message
+  read -p "Enter a commit message: " commit_message
 
-# Commit with the provided message
-git commit -m "$commit_message"
+  # Commit with the provided message
+  git commit -m "$commit_message"
 
-# Push the changes to the remote repository
-git push
+  # Push the changes to the remote repository
+  git push
+fi
+
